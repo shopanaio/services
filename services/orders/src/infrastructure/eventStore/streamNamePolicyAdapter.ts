ee0940205e110341fce75ce35f9509f7b28d59f5
phl <philipp.sapronov@gmail.com>
2025-09-22 01:01:21 +0300

    Update stream name format in StreamNamePolicyAdapter to use 'ord-' prefix for order streams.

--- services/orders/src/infrastructure/eventStore/streamNamePolicyAdapter.ts
+++ services/orders/src/infrastructure/eventStore/streamNamePolicyAdapter.ts
@@ -2,6 +2,6 @@ import type { StreamNamePolicyPort } from "@src/application/ports/streamNamePort
 
 export class StreamNamePolicyAdapter implements StreamNamePolicyPort {
   buildOrderStreamNameFromId(id: string): string {
-    return `chk-${id}`;
+    return `ord-${id}`;
   }
 }
