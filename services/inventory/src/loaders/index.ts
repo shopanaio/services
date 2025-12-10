2604a7c796ec26de8d394a944508dfacfbc00a90
phl <philipp.sapronov@gmail.com>
2025-12-10 14:35:01 +0200

    refactor

--- services/inventory/src/loaders/index.ts
+++
@@ -1,6 +0,0 @@
-export { VariantLoader } from "./VariantLoader.js";
-export { ProductLoader } from "./ProductLoader.js";
-export { OptionLoader } from "./OptionLoader.js";
-export { FeatureLoader } from "./FeatureLoader.js";
-export { WarehouseLoader } from "./WarehouseLoader.js";
-export { Loader } from "./Loader.js";
