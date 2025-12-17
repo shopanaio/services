c44202a169b9f068445590df9e551fcf137ca566
phl <philipp.sapronov@gmail.com>
2025-12-17 18:12:01 +0200

    update users scheme

--- services/users/src/api/graphql-admin/resolvers/mutations/user.ts
+++ services/users/src/api/graphql-admin/resolvers/mutations/user.ts
@@ -1,131 +1,72 @@
-import { parseGraphqlInfo } from "@shopana/type-resolver";
 import type { ServiceContext } from "../../../../context/index.js";
-import { UserResolver } from "../../../../resolvers/admin/UserResolver.js";
-import {
-  UserCreateScript,
-  UserDeleteScript,
-  UserUpdateScript,
-} from "../../../../scripts/user/index.js";
-import { noKernelError, requireContext } from "../utils.js";
 
-interface UserCreateInput {
-  email: string;
-  password: string;
+interface UserUpdateProfileInput {
   firstName?: string | null;
   lastName?: string | null;
   phone?: string | null;
   locale?: string | null;
-  isAdmin?: boolean | null;
-  roles?: string[] | null;
 }
 
-interface UserUpdateInput {
-  id: string;
-  email?: string | null;
-  firstName?: string | null;
-  lastName?: string | null;
-  phone?: string | null;
-  locale?: string | null;
-  isAdmin?: boolean | null;
-  isForbidden?: boolean | null;
-  roles?: string[] | null;
+interface UserUpdateEmailInput {
+  newEmail: string;
+  currentPassword: string;
 }
 
-interface UserDeleteInput {
-  id: string;
-  permanent?: boolean | null;
+interface UserUpdatePasswordInput {
+  currentPassword: string;
+  newPassword: string;
 }
 
 export const userMutationResolvers = {
   Mutation: {
-    userCreate: async (
+    userUpdateProfile: async (
       _parent: unknown,
-      { input }: { input: UserCreateInput },
-      ctx: ServiceContext,
-      info: unknown
+      { input: _input }: { input: UserUpdateProfileInput },
+      _ctx: ServiceContext
     ) => {
-      if (!ctx.kernel) {
-        return noKernelError({ user: null });
-      }
-
-      const result = await ctx.kernel.runScript(UserCreateScript, {
-        email: input.email,
-        password: input.password,
-        firstName: input.firstName ?? undefined,
-        lastName: input.lastName ?? undefined,
-        phone: input.phone ?? undefined,
-        language: input.locale ?? undefined,
-        isAdmin: input.isAdmin ?? undefined,
-        roles: input.roles ?? undefined,
-      });
-
-      const userFieldInfo = parseGraphqlInfo(info, "user");
-
+      // TODO: implement
       return {
-        user: result.userId
-          ? await UserResolver.load(
-              result.userId,
-              userFieldInfo,
-              requireContext(ctx)
-            )
-          : null,
-        userErrors: result.userErrors,
+        user: null,
+        userErrors: [
+          {
+            code: "NOT_IMPLEMENTED",
+            message: "userUpdateProfile is not implemented yet",
+          },
+        ],
       };
     },
 
-    userUpdate: async (
+    userUpdateEmail: async (
       _parent: unknown,
-      { input }: { input: UserUpdateInput },
-      ctx: ServiceContext,
-      info: unknown
+      { input: _input }: { input: UserUpdateEmailInput },
+      _ctx: ServiceContext
     ) => {
-      if (!ctx.kernel) {
-        return noKernelError({ user: null });
-      }
-
-      const result = await ctx.kernel.runScript(UserUpdateScript, {
-        id: input.id,
-        email: input.email ?? undefined,
-        firstName: input.firstName ?? undefined,
-        lastName: input.lastName ?? undefined,
-        phone: input.phone ?? undefined,
-        language: input.locale ?? undefined,
-        isAdmin: input.isAdmin ?? undefined,
-        isForbidden: input.isForbidden ?? undefined,
-        roles: input.roles ?? undefined,
-      });
-
-      const userFieldInfo = parseGraphqlInfo(info, "user");
-
+      // TODO: implement
       return {
-        user: result.userId
-          ? await UserResolver.load(
-              result.userId,
-              userFieldInfo,
-              requireContext(ctx)
-            )
-          : null,
-        userErrors: result.userErrors,
+        user: null,
+        userErrors: [
+          {
+            code: "NOT_IMPLEMENTED",
+            message: "userUpdateEmail is not implemented yet",
+          },
+        ],
       };
     },
 
-    userDelete: async (
+    userUpdatePassword: async (
       _parent: unknown,
-      { input }: { input: UserDeleteInput },
-      ctx: ServiceContext
+      { input: _input }: { input: UserUpdatePasswordInput },
+      _ctx: ServiceContext
     ) => {
-      if (!ctx.kernel) {
-        return noKernelError({ deletedUserId: null });
-      }
-
-      const result = await ctx.kernel.runScript(UserDeleteScript, {
-        id: input.id,
-        permanent: input.permanent ?? undefined,
-      });
-
+      // TODO: implement
       return {
-        deletedUserId: result.deletedUserId ?? null,
-        userErrors: result.userErrors,
+        success: false,
+        userErrors: [
+          {
+            code: "NOT_IMPLEMENTED",
+            message: "userUpdatePassword is not implemented yet",
+          },
+        ],
       };
     },
   },
--- services/users/src/scripts/user/index.ts
+++ services/users/src/scripts/user/index.ts
@@ -1,7 +1,2 @@
-// User scripts
-export { UserCreateScript } from "./UserCreateScript.js";
-export { UserUpdateScript } from "./UserUpdateScript.js";
-export { UserDeleteScript } from "./UserDeleteScript.js";
-
 // DTOs
 export * from "./dto/index.js";
