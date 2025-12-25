856fb18bf3ec97319ce1099dfc4a60180af89634
phl <philipp.sapronov@gmail.com>
2025-12-25 17:25:21 +0200

    fix

--- services/iam/src/infrastructure/db/database.ts
+++ services/iam/src/infrastructure/db/database.ts
@@ -1,6 +1,6 @@
 import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as schema from "../repositories/models/index.js";
+import * as schema from "../../repositories/models/index.js";
 
 type DrizzleDatabase = PostgresJsDatabase<typeof schema>;
 
