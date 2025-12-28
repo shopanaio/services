9888fd2964cc21b610aa826b07d22a90b8d3894f
phl <philipp.sapronov@gmail.com>
2025-12-28 23:25:07 +0200

    remove rbac

--- services/iam/src/constants/index.ts
+++ services/iam/src/constants/index.ts
@@ -1,12 +1,1 @@
-export {
-  ORGANIZATION_RESOURCES,
-  IAM_SERVICE_RESOURCES,
-  PREDEFINED_ROLES,
-  ROLE_DISPLAY_NAMES,
-  ROLE_DESCRIPTIONS,
-  ROLE_PERMISSIONS,
-  CACHE_TTL,
-  CACHE_KEYS,
-} from "./rbac.js";
-
-export type { PredefinedRoleName, RolePermissionDef } from "./rbac.js";
+export { CACHE_TTL, CACHE_KEYS } from "./rbac.js";
