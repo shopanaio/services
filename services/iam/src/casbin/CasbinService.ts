7d808f4652da05c50694cb79aa58e53ea8596681
phl <philipp.sapronov@gmail.com>
2025-12-29 07:36:59 +0200

    fix casbin

--- services/iam/src/casbin/CasbinService.ts
+++ services/iam/src/casbin/CasbinService.ts
@@ -47,6 +47,9 @@ p = sub, dom, obj, act
 [role_definition]
 g = _, _, _
 
+[policy_effect]
+e = some(where (p.eft == allow))
+
 [matchers]
 m = g(r.sub, p.sub, r.dom) && keyMatch(r.dom, p.dom) && keyMatch(r.obj, p.obj) && keyMatch(r.act, p.act)
 `.trim();
