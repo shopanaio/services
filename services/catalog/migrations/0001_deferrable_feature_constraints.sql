d6c79b674e71e32df7e5ed0d77903f3331d119f0
Philipp Sapronov <49458012+philipp-sapronov@users.noreply.github.com>
2026-02-06 19:13:23 +0200

    Rename catalog schema from inventory to catalog

--- services/catalog/migrations/0001_deferrable_feature_constraints.sql
+++ services/catalog/migrations/0001_deferrable_feature_constraints.sql
@@ -1,15 +1,16 @@
-ALTER TABLE "inventory"."product_feature" DROP CONSTRAINT "product_feature_parent_id_product_feature_id_fk";
+-- Custom SQL migration file, put your code below! --
+ALTER TABLE "catalog"."product_feature" DROP CONSTRAINT "product_feature_parent_id_product_feature_id_fk";
 --> statement-breakpoint
-ALTER TABLE "inventory"."product_feature_value" DROP CONSTRAINT "product_feature_value_feature_id_product_feature_id_fk";
+ALTER TABLE "catalog"."product_feature_value" DROP CONSTRAINT "product_feature_value_feature_id_product_feature_id_fk";
 --> statement-breakpoint
-ALTER TABLE "inventory"."product_feature" DROP CONSTRAINT "product_feature_product_id_index_uniq";
+ALTER TABLE "catalog"."product_feature" DROP CONSTRAINT "product_feature_product_id_index_uniq";
 --> statement-breakpoint
-ALTER TABLE "inventory"."product_feature" ADD CONSTRAINT "product_feature_parent_id_product_feature_id_fk" FOREIGN KEY ("parent_id") REFERENCES "inventory"."product_feature"("id") ON DELETE CASCADE ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE "catalog"."product_feature" ADD CONSTRAINT "product_feature_parent_id_product_feature_id_fk" FOREIGN KEY ("parent_id") REFERENCES "catalog"."product_feature"("id") ON DELETE CASCADE ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED;
 --> statement-breakpoint
-ALTER TABLE "inventory"."product_feature_value" DROP CONSTRAINT "product_feature_value_feature_id_index_uniq";
+ALTER TABLE "catalog"."product_feature_value" DROP CONSTRAINT "product_feature_value_feature_id_index_uniq";
 --> statement-breakpoint
-ALTER TABLE "inventory"."product_feature_value" ADD CONSTRAINT "product_feature_value_feature_id_product_feature_id_fk" FOREIGN KEY ("feature_id") REFERENCES "inventory"."product_feature"("id") ON DELETE CASCADE ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE "catalog"."product_feature_value" ADD CONSTRAINT "product_feature_value_feature_id_product_feature_id_fk" FOREIGN KEY ("feature_id") REFERENCES "catalog"."product_feature"("id") ON DELETE CASCADE ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED;
 --> statement-breakpoint
-ALTER TABLE "inventory"."product_feature" ADD CONSTRAINT "product_feature_product_id_index_uniq" UNIQUE ("product_id", "index") DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE "catalog"."product_feature" ADD CONSTRAINT "product_feature_product_id_index_uniq" UNIQUE ("product_id", "index") DEFERRABLE INITIALLY DEFERRED;
 --> statement-breakpoint
-ALTER TABLE "inventory"."product_feature_value" ADD CONSTRAINT "product_feature_value_feature_id_index_uniq" UNIQUE ("feature_id", "index") DEFERRABLE INITIALLY DEFERRED;
+ALTER TABLE "catalog"."product_feature_value" ADD CONSTRAINT "product_feature_value_feature_id_index_uniq" UNIQUE ("feature_id", "index") DEFERRABLE INITIALLY DEFERRED;
