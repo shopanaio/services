9bd534d5518d997aea3eab6bb777d11340380ab1
Philipp Sapronov <49458012+philipp-sapronov@users.noreply.github.com>
2026-07-09 01:03:57 +0300

    Refactor product connection resolver to use base classes

--- services/catalog/src/resolvers/service/ProductConnectionResolver.ts
+++ services/catalog/src/resolvers/service/ProductConnectionResolver.ts
@@ -1,11 +1,21 @@
 import type { ProductConnectionInput } from "../../repositories/product/ProductRepository.js";
-import type { ConnectionData } from "../admin/connection/BaseConnectionResolver.js";
+import {
+  BaseConnectionEdgeResolver,
+  BaseConnectionResolver,
+  type ConnectionData,
+  type EdgeData,
+} from "./connection/BaseConnectionResolver.js";
 import { ProductSnapshotResolver } from "./ProductSnapshotResolver.js";
-import { ServiceType } from "./ServiceType.js";
 
-export class ServiceProductConnectionResolver extends ServiceType<
+class ServiceProductEdgeResolver extends BaseConnectionEdgeResolver<ProductSnapshotResolver> {
+  node(): ProductSnapshotResolver {
+    return new ProductSnapshotResolver(this.$props.nodeId, this.$ctx);
+  }
+}
+
+export class ServiceProductConnectionResolver extends BaseConnectionResolver<
   ProductConnectionInput,
-  ConnectionData
+  ServiceProductEdgeResolver
 > {
   async $preload(): Promise<ConnectionData> {
     return this.$ctx.kernel
@@ -13,25 +23,7 @@ export class ServiceProductConnectionResolver extends ServiceType<
       .repository.product.getConnection(this.$props);
   }
 
-  protected async createNodeResolver(nodeId: string) {
-    return new ProductSnapshotResolver(nodeId, this.$ctx);
-  }
-
-  async edges() {
-    const edgesData = await this.$get("edges");
-    return Promise.all(
-      (edgesData ?? []).map(async (edge) => ({
-        cursor: edge.cursor,
-        node: await this.createNodeResolver(edge.nodeId),
-      }))
-    );
-  }
-
-  async pageInfo() {
-    return this.$get("pageInfo");
-  }
-
-  async totalCount(): Promise<number> {
-    return (await this.$get("totalCount")) ?? 0;
+  protected createEdgeResolver(edge: EdgeData): ServiceProductEdgeResolver {
+    return new ServiceProductEdgeResolver(edge, this.$ctx);
   }
 }
---
+++ services/catalog/src/resolvers/service/connection/BaseConnectionResolver.ts
@@ -0,0 +1,49 @@
+import { type PageInfo } from "@shopana/drizzle-query";
+import { ServiceType } from "../ServiceType.js";
+
+export interface EdgeData {
+  cursor: string;
+  nodeId: string;
+}
+
+export interface ConnectionData {
+  edges: EdgeData[];
+  pageInfo: PageInfo;
+  totalCount: number;
+}
+
+export abstract class BaseConnectionEdgeResolver<
+  TNode,
+> extends ServiceType<EdgeData, EdgeData> {
+  protected $preload(): EdgeData {
+    return this.$props;
+  }
+
+  cursor(): string {
+    return this.$props.cursor;
+  }
+
+  abstract node(): TNode | Promise<TNode>;
+}
+
+export abstract class BaseConnectionResolver<
+  TArgs = unknown,
+  TEdge extends BaseConnectionEdgeResolver<unknown> = BaseConnectionEdgeResolver<unknown>,
+> extends ServiceType<TArgs, ConnectionData> {
+  abstract $preload(): Promise<ConnectionData>;
+
+  protected abstract createEdgeResolver(edge: EdgeData): TEdge;
+
+  async edges(): Promise<TEdge[]> {
+    const edgesData = await this.$get("edges");
+    return (edgesData ?? []).map((edge) => this.createEdgeResolver(edge));
+  }
+
+  async pageInfo() {
+    return this.$get("pageInfo");
+  }
+
+  async totalCount(): Promise<number> {
+    return (await this.$get("totalCount")) ?? 0;
+  }
+}
