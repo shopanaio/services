e8a2f01925e69ee30d6cd9001a099fe4564c9f6f
Philipp Sapronov <49458012+philipp-sapronov@users.noreply.github.com>
2026-07-14 13:52:26 +0300

    Add product relation control registration

--- admin/src/layouts/filters/components/relation-control/register.ts
+++ admin/src/layouts/filters/components/relation-control/register.ts
@@ -1,9 +1,11 @@
 "use client";
 
 import "@/shared/components/entity-picker-modal/configs/category-picker-config";
+import "@/shared/components/entity-picker-modal/configs/product-picker-config";
 import "@/shared/components/entity-picker-modal/configs/vendor-picker-config";
 import { EntityPickerRelationControl } from "./entity-picker-relation-control";
 import { relationControlRegistry } from "./registry";
 
 relationControlRegistry.register("category", EntityPickerRelationControl);
+relationControlRegistry.register("product", EntityPickerRelationControl);
 relationControlRegistry.register("vendor", EntityPickerRelationControl);
