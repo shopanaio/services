3c5683581dd5e97afbbaf0fc69545f13ca4a29f7
Philipp Sapronov <49458012+philipp-sapronov@users.noreply.github.com>
2026-07-22 19:27:28 +0300

    Add general settings layout and IAM auth env examples

--- admin/src/domains/system/general-settings/page/page.tsx
+++ admin/src/domains/system/general-settings/page/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { DataLayout } from "@/layouts/data";
+
 export default function GeneralSettingsPage() {
-  return null;
+  return (
+    <DataLayout name="general-settings" title="General settings">
+      {null}
+    </DataLayout>
+  );
 }
