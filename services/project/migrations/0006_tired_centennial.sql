a50893fc5d57781ea8ba01d1468de5e2fa283215
Philipp Sapronov <49458012+philipp-sapronov@users.noreply.github.com>
2026-07-25 22:55:50 +0300

    Add service implementation and supporting configuration

---
+++ services/project/migrations/0006_tired_centennial.sql
@@ -0,0 +1,4 @@
+DROP TABLE "store"."sales_channel" CASCADE;--> statement-breakpoint
+DROP TABLE "store"."market_sales_channel" CASCADE;--> statement-breakpoint
+DROP TYPE "store"."sales_channel_status";--> statement-breakpoint
+DROP TYPE "store"."sales_channel_type";
