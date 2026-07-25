d959314089235ee2740db27c794ca985300a4999
Philipp Sapronov <49458012+philipp-sapronov@users.noreply.github.com>
2026-07-25 22:22:34 +0300

    Remove obsolete admin app and sales channel resolvers

--- services/apps/src/api/graphql-admin/resolvers/index.ts
+++ services/apps/src/api/graphql-admin/resolvers/index.ts
@@ -1,9 +1,7 @@
 import { MutationResolver } from "../../../resolvers/admin/MutationResolver.js";
 import { QueryResolver } from "../../../resolvers/admin/QueryResolver.js";
-import { typeResolvers } from "./types.js";
 
 export const resolvers = {
   Query: QueryResolver,
   Mutation: MutationResolver,
-  ...typeResolvers,
 };
